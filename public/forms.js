af3cb7f2b2f90cbd965ba6bb9d2087a40b79d650
Mateo Estrada <mateoej12@gmail.com>
2021-02-08 09:10:07 -0800

    Added a search functionality for borrowers.

--- public/forms.js
+++ public/forms.js
@@ -1,16 +1,49 @@
-// This is where the back-end of the forms will be located.
+// This is where the dynamic elements of the forms will be located.
 
 var formAdd = document.getElementById("bookForm");
 var formButton = document.getElementById("submitBook")
 
 var borrowBook = document.getElementById("status");
-
-
 if (borrowBook){
     if (borrowBook.nodeValue == "Yes"){
         console.log("WHo is renting it?")
     }
 }
 
-formButton.setAttribute(onlick, "return confirm('Are you sure you want to add this book?');");
+formButton.setAttribute('onclick', "return confirm('Are you sure you want to add this book?');");
+
+
+// making the form appear/disappear
+
+document.getElementById("bookFormDiv").setAttribute('style', 'display: None');
+
+function openForm() {
+    document.getElementById("bookFormDiv").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("bookFormDiv").style.display = "none";
+}
+
+
+function openAuthorForm(){
+    document.getElementById("authorForm").style.display = "block";
+}
+
+
+function closeAuthorForm(){
+    document.getElementById("authorForm").style.display = "none";
+}
+
+
+function openBorrowForm(){
+    document.getElementById("borrowForm").style.display = "block";
+}
+
+function closeBorrowForm(){
+    document.getElementById("borrowForm").style.display = "none";
+}
+// appearBtn.setAttribute("onclick", "closeForm()")
+// formAdd.appendChild(appearBtn);
+
 
